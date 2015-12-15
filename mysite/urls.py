4f098bd0a9673bfb8bec3334c4b2d01d99aa9958
jernacotillon <cotillonjerna@gmail.com>
2015-12-15 09:00:31 +0000

    Remove Head. Files merge

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-=======
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
->>>>>>> 1b30612c0046178ecf21f89ab435590cddefd6df
-]
+    ]
