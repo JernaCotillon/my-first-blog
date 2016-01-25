a55b07f9993d36a985090678f9957106ddf57243
jernacotillon <cotillonjerna@gmail.com>
2016-01-24 20:55:17 -0500

    Adding New url for outputing post with json format

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-
+    url(r'^json-posts/(?P<pk>\S+)/$', json_format.json_default_posts, name="page_json_default_post"),
 ]
